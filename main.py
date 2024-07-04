18be04c2de75bd708bf07855699c65394ed4a80b
luisnoda <luisnoda023@gmail.com>
2024-07-04 17:48:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,55 @@
 from pygame import * 
+from random import randint 
 
 init()
 
-window_WIDTH, window_HEIGHT = 1000, 800 
-CHAR_IMG = "char.png"
-VOLVASOR_IMG = "volvasor.png"
-GOAL_IMG = "hero.png"
-FAIL_IMG = "fail_1.jpg"
+
+
+#1 ESTABLECER EL MAIN SCREEN 
+SCREEN_WIDTH, SCREEN_HEIGHT = 1000, 800 
+X_CORD, Y_CORD = (SCREEN_WIDTH - 100)// 2, (SCREEN_HEIGHT - 100)// 2
+BACKGROUND_IMG  = "fondoespacial.jpg"
+PLAYER_IMG = "falco.png"
+ENEMY_IMG = "tie.png" 
+GAME_OVER = "gameover2.jpg"
+BULLET_IMG = "bala2.png"
+WIN_IMG = "victoria.jpg"
+BACKGROUND_SOUND = "musica.mp3"
+FIRE_SOUND = "sonidobala.mp3"
 BLACK = (0,0,0)
+YELLOW = (227, 252, 3)
 WHITE = (255,255,255)
 BLUE = (50, 117, 168)
 GREEN = (54, 168, 50)
 PURPLE = (141, 50, 168)
-window = display.set_mode ((window_WIDTH, window_HEIGHT))
-display.set_caption ("Proyecto Noda")
+
+#FUNCIONALIDADES 
+score = 0
+misses = 0
+
+#INICIALIZAR EL MODULO DE FUENTES 
+font.init()
+font_1 = font.Font(None, 40)
+
+#inicializar modulo de sonido
+mixer.init()
+#CARGAR MUSICA DE FONDO
+mixer.music.load(BACKGROUND_SOUND)
+#REPRODUCIR MUSICA DE FONDO
+mixer.music.play()
+
+fire_sound = mixer.Sound(FIRE_SOUND)
 
 
 
+
+
+#ventana principal
+screen = display.set_mode ((SCREEN_WIDTH, SCREEN_HEIGHT))
+display.set_caption ("Tirador")
+background = transform.scale(image.load(BACKGROUND_IMG),(SCREEN_WIDTH, SCREEN_HEIGHT))
+
+#definicion de clases 
 class Character(sprite.Sprite):
     def __init__(self, char_image, x_cord, y_cord,char_width, char_height, speed=0):
         super().__init__() 
@@ -29,7 +62,7 @@ class Character(sprite.Sprite):
         self.speed = speed
 
     def reset(self):
-        window.blit(self.image,(self.rect.x, self.rect.y))
+        screen.blit(self.image,(self.rect.x, self.rect.y))
 
 class Player(Character):
 
@@ -39,92 +72,116 @@ class Player(Character):
 
         if keys[K_a] and self.rect.x > 0:
             self.rect.x -= self.speed
-        elif keys[K_d] and self.rect.x < window_WIDTH - self.widht:
+        elif keys[K_d] and self.rect.x < SCREEN_WIDTH - self.widht:
             self.rect.x += self.speed
+    
+    def fire(self):
+        #CREAR BALAS
+        print("pew pew")
+        fire_sound.play()
+        bullet = Bullet(BULLET_IMG, self.rect.centerx, self.rect.top, 30, 30, 10 )
+        bullets.add(bullet)
         
-        elif keys[K_w] and self.rect.y > 0:
-            self.rect.y -= self.speed 
-        
-        elif keys[K_s] and self.rect.y < window_HEIGHT - self.height:
-            self.rect.y += self.speed
 
 class Enemy(Character):
-    def __init__(self,char_image, x_cord, y_cord,char_width, char_height, speed):
-        super().__init__(char_image, x_cord, y_cord, char_width, char_height, speed)
-        self.move_right = True
-        
     def update(self):
-        if self.move_right:
-            self.rect.x += self.speed
-            if self.rect.x >= window_WIDTH - self.widht:
-                self.move_right = False 
-        else: # cuando self.move.right == false
-            self.rect.x -= self.speed 
-            if self.rect.x <= 0:
-                self.move_right = True 
-        
-class Wall(sprite.Sprite):
-    def __init__(self, color, x_cord, y_cord, wall_width, wall_height):
-        super().__init__()
-        self.color = color 
-        self.width = wall_width
-        self.height = wall_height
-        
-        #definimos una propiedad imagen, un rectangulo
-        self.image = Surface([self.width, self.height])
-        self.image.fill(self.color)#rellenamos de color el metodo fill
-        #cada obejto debe almacenar la propiedad rect
-        self.rect = self.image.get_rect()
-        self.rect.x = x_cord
-        self.rect.y = y_cord
+        global misses
+        self.rect.y += self.speed 
+
+        if self.rect.y >= SCREEN_HEIGHT:
+            print(misses)   
+            self.rect.y = 0 - self.height
+            self.rect.x = randint(0, SCREEN_WIDTH - self.widht)
+            self.speed = randint(1, 6)
+            misses += 1
+
+class Bullet(Character):
+    def update(self):
+        self.rect.y -= self.speed
+
+        if self.rect.y  < 0: 
+            self.kill()
+
     
-    def draw_wall(self):
-        draw.rect(window,self.color,self.rect)
-#instancias 
-pokemon = Player(CHAR_IMG, 100,100, 60, 60, 3)
-volvasor = Enemy(VOLVASOR_IMG, 500 ,100,30,50, 5)
-goal = Character(GOAL_IMG, 500, 400, 50, 50)
-wall_1 = Wall(WHITE, 300, 100, 400, 20 )
-wall_2 = Wall(GREEN, 45, 55,200, 20)
-wall_3 = Wall(BLUE, 260, 55, 20, 200)
-wall_4 = Wall(PURPLE, 458, 44, 66, 600)
 
 
-clock = time.Clock() 
+#intancias de clases objetos
+player = Player(PLAYER_IMG, 470, 720, 60, 80, 5)
+monsters = sprite.Group() # creando un grupo de objetos enemigos
 
+for i in range(1, 6):
+#creando una instancia de la clase enemy
+    monster = Enemy(ENEMY_IMG, randint(0,SCREEN_WIDTH - 60), 0, 50, 50, randint(1, 6))
+# añadiendo un enemigo creado al grupo Monster
+    monsters.add(monster)
+
+bullets = sprite.Group() #creamos un grupo de objeto de bullets
+
+#ciclo de juego 
 run = True
-finish = False  
+finish = False 
+clock = time.Clock()
 while run:
 
-    # 3. MANEJO DE EVENTOS 
-    for i in event.get():
-        if i.type == QUIT: 
+    for e in event.get():
+        if e.type == QUIT:
             run = False
-    
-    if not finish:
-        window.fill(BLACK)
-        pokemon.reset()
-        pokemon.update()
-        volvasor.reset()
-        volvasor.update()
-        wall_1.draw_wall()
-        wall_2.draw_wall()
-        wall_3.draw_wall()
-        wall_4.draw_wall()
-        goal.reset()
 
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                player.fire()
+                
+                
+
+    if not finish:
+
+
+        screen.fill(BLACK)
+
+        screen.blit(background,(0,0))
+        score_text = font_1.render(f"SCORE: {score}", True, YELLOW )
+        
+        screen.blit (score_text,(SCREEN_WIDTH //25, SCREEN_HEIGHT //25))
+        player.reset()
+        player.update()
+        monsters.draw(screen)
+        monsters.update()
+        bullets.draw(screen)
+        bullets.update()
+        
+        #colisiones 
+        collides = sprite.groupcollide(monsters, bullets, True, True)
+
+        for collide in collides:
+            score += 1
+            monster = Enemy(ENEMY_IMG, randint(0,SCREEN_WIDTH - 60), 0, 50, 50, randint(1, 6))
+            monsters.add(monster)
+        
         #condiciones de derrota 
-        if sprite.collide_rect(pokemon, volvasor) or sprite.collide_rect(pokemon, wall_1) or sprite.collide_rect(pokemon, wall_2) or sprite.collide_rect(pokemon, wall_3) or sprite.collide_rect(pokemon, wall_4): 
-            print("OUCH")
+        if misses == 10 or sprite.spritecollide(player, monsters, False): 
             finish = True
-            window.fill(BLACK)
-            fail_img = transform.scale(image.load(FAIL_IMG), (1000, 800  ))
-            window.blit(fail_img, (0,0)) 
+            screen.fill(BLACK)
+            GAME_OVER = transform.scale(image.load(GAME_OVER), (1000,800))
+            screen.blit(GAME_OVER, (0,0)) 
         
+        if score == 5:
+            finish = True
+            screen.fill(BLACK)
+            WIN_IMG = transform.scale(image.load(WIN_IMG), (1000,800))
+            screen.blit(WIN_IMG, (0,0))
+            
+
         
+        #enemy.reset()
+        #enemy.update()
+    
     display.update()
     clock.tick(60)
+
+
+
 quit()
+
 
 
         
